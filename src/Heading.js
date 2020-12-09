6e8822045792064032b0dcb4a65e0b7ca18927bd
Jonathan Warner <jewarner57@gmail.com>
2020-12-09 18:17:38 -0500

    Challenge 2 Complete

---
+++ src/Heading.js
@@ -0,0 +1,18 @@
+function Heading() {
+    return (
+        <div className="Heading">
+            <header className="header">
+                <div className="inner-heading">
+                    <div className="headerName">
+                        <h1 >Jonathan Warner</h1>
+                    </div>
+                    <div className="headerAssignment">
+                        <h1 >FEW 1.2 Final Assignment</h1>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Heading;
